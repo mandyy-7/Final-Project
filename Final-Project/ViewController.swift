0168ed650b4fe2730c1aaf64189bdf389fe6e6b1
Mandy Yuan <myuan2024@gmail.com>
2021-07-20 12:59:41 -0500

    created label!

---
+++ Final-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final-Project
+//
+//  Created by Mandy Yuan on 7/20/21.
+//  Copyright © 2021 Mandy Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
